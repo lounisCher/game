81395d0ee6d84c2c1093df6e1b03011dd81d4480
Lounis <lounischerifi29@gmail.com>
2024-09-21 22:46:24 +0200

    platform selector

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCropedImageUrl from "../services/imageUrl";
 
 interface Props{
   onSelectGenre: (genre: Genre)=>void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre}:Props) => {
+const GenreList = ({onSelectGenre, selectedGenre}:Props) => {
     const {data, isLoading, errors} = useGenres();
     if(errors) return null;
     if(isLoading) return <Spinner/>
@@ -17,7 +18,8 @@ const GenreList = ({onSelectGenre}:Props) => {
             <ListItem key={genre.id} paddingY={2}>
                 <HStack>
                 <Image src={getCropedImageUrl(genre.image_background)} boxSize="32px" borderRadius="8px"/>
-                 <Button onClick={()=>onSelectGenre(genre)} variant='link'>
+                 <Button onClick={()=>onSelectGenre(genre)} variant='link'
+                  fontWeight={genre.id===selectedGenre?.id?"bold":"normal"}>
                   {genre.name}
                   </Button>
                 </HStack>
